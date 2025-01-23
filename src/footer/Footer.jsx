ec841e5be8e9f6400a69a8914a6f356f5027bfa1
SamuelReyes17 <samyreyese@hotmail.com>
2025-01-23 14:41:28 -0500

    add src folders

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./footer.css"
+const Footer = () => {
+    return (
+        <footer className="footer-container">Aca el footer</footer>
+    );
+};
+
+export default Footer;
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.css"
+const Navbar = ()=>{
+    return (
+        <nav>
+            <h2>Prosox</h2>
+            <ul>
+                <li>Shop</li>
+                <li>Best Seller</li>
+                <li>Reviews</li>
+            </ul>
+        </nav>
+    );
+};
+export default Navbar;
+ 
