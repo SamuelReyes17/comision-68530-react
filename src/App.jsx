f9237d0c6160bcc9f183975bdbbb19b517af951a
SamuelReyes17 <samyreyese@hotmail.com>
2025-01-31 22:51:58 -0500

    add organization folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Home from "./home/Home";
-import Footer from "./footer/Footer";
-import MyNavbar from "./navbar/Navbar";
+import Home from "./components/pages/home/Home";
+import Footer from "./components/layouts/footer/Footer";
+import MyNavbar from "./components/layouts/navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
---
+++ src/products.js
@@ -0,0 +1,65 @@
+export const products = [
+    {
+        id: "1",
+        title: "Black shin pad sleeves",
+        price: 29.99,
+        stock: 200,
+        categroy: "shin pads",
+        description: "super light modern shin pads",
+        imageUrl: ""
+    },
+    {
+        id: "2",
+        title: "White soft shin pads",
+        price: 24.99,
+        stock: 100,
+        categroy: "shin pads",
+        description: "ight modern and soft shin pads",
+        imageUrl: ""
+    },
+    {
+        id: "1",
+        title: "Black shin pad sleeves",
+        price: 29.99,
+        stock: 200,
+        categroy: "shin pads",
+        description: "super light modern shin pads",
+        imageUrl: ""
+    },
+    {
+        id: "3",
+        title: "White shin pads flex",
+        price: 29.99,
+        stock: 50,
+        categroy: "shin pads",
+        description: "moldeable shin pads for pros",
+        imageUrl: ""
+    },
+    {
+        id: "4",
+        title: "Mini shin pads",
+        price: 20.99,
+        stock: 150,
+        categroy: "shin pads",
+        description: "super light and tiny modern shin pads",
+        imageUrl: ""
+    },
+    {
+        id: "5",
+        title: "Grip Tape",
+        price: 14.99,
+        stock: 30,
+        categroy: "accesories",
+        description: "tape for shin pads",
+        imageUrl: ""
+    },
+    {
+        id: "6",
+        title: "Sock tape",
+        price: 9.99,
+        stock: 200,
+        categroy: "accesories",
+        description: "easy good material tape",
+        imageUrl: ""
+    }
+]
